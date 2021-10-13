be8457fd34978e0b797af877164575e169b9d061
Nirpendra09 <nirpendra09@gmail.com>
2021-10-13 09:08:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Switch, Route } from "react-router";
+
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS Page</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
