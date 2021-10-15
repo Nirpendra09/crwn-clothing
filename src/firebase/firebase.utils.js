9deb22dd86b2afcdc716168d86452a540a5d33fe
Nirpendra Chaudhary <nirpendra09@gmail.com>
2021-10-15 15:11:16 +0000

    integrated firebase, created signin with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const config = {
+  apiKey: "AIzaSyC9l1Iaz1SOGX_0O0guEX5tjiNB_6hZYqc",
+  authDomain: "clothingstoredb-f6e71.firebaseapp.com",
+  projectId: "clothingstoredb-f6e71",
+  storageBucket: "clothingstoredb-f6e71.appspot.com",
+  messagingSenderId: "1048721584337",
+  appId: "1:1048721584337:web:c10d616fd92f29ec2295f0",
+  measurementId: "G-1Q8BQ5BT1F",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
